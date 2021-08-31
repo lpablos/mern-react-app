d18a9bb9ad56cfd2289317667a91a902b17b4415
Your Name <you@example.com>
2021-08-30 20:59:08 -0500

    Apartado grafico del cliente

--- client/src/components/proyectos/Proyectos.jsx
+++ client/src/components/proyectos/Proyectos.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Siderbar from '../layout/Siderbar'
+import Barra from '../layout/Barra'
+import FormTareas from '../tareas/FormTareas'
+import ListadoTareas from '../tareas/ListadoTareas'
 
 const Proyectos = () => {
     return (
         <div className="contenedor-app">
             <Siderbar/>
             <div className="seccion-principal">
+                <Barra/>
                 <main>
+                    <FormTareas/>
                     <div className="contenedor-tareas">
-
+                        <ListadoTareas/>
                     </div>
                 </main>
             </div>
---
+++ client/src/components/tareas/ListadoTareas.jsx
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react'
+import Tarea from './Tarea'
+
+const ListadoTareas = () => {
+    const tareasProyecto = [
+        {
+            nombre: 'Elegir Plataforma',
+            estado: true
+        },
+        {
+            nombre: 'Elegir Colores',
+            estado: false
+        },
+        {
+            nombre: 'Elegir Plataformas de Pago',
+            estado: true
+        },
+        {
+            nombre: 'Elegir Hosting',
+            estado: false
+        },
+    ]
+    return (
+        <Fragment>
+            <h2>Proyectos : Tienda virtual</h2>
+            <ul className="listado-tareas">
+                {
+                    tareasProyecto.length === 0 
+                    ?(<li className="tarea"><p>No hay tareas</p></li>)
+                    :tareasProyecto.map(tarea=>(
+                        <Tarea 
+                            tarea={tarea}
+                        />
+                    ))
+                }
+            </ul>
+            <button type="button" className="btn btn-eliminar sombra">Eliminar Proyecto</button>
+
+        </Fragment>
+    )
+}
+
+export default ListadoTareas
