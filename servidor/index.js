a1fbc64d7914c712a29d3efea50b513a3d20d365
Luis Jorge <gevolutionxalapa@gmail.com>
2021-10-20 13:10:20 -0500

    Fix

--- servidor/index.js
+++ servidor/index.js
@@ -9,19 +9,12 @@ conectarDB()
 
 // Habilitar cors
 app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 // Habilitar express JSON
 app.use( express.json({ extended: true }) )
 
 // puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // importar rutas
 // Registro de usuario
